6918b2b9ea615c94a044ce39b518858f0619aac5
Trung Dinh Vu <trungdvu@kms-technology.com>
2022-02-08 01:10:55 +0700

    Add login/signup page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,57 @@
 import React, { FC } from 'react';
+import { Button, Form, Input, Layout } from 'antd';
+import { Link } from 'react-router-dom';
 
 const LoginPage: FC = () => {
-  return <div>This is Login Page</div>;
+  const onFinish = (values: any) => {
+    console.log('Success :>> ', values);
+  };
+
+  return (
+    <Layout className="p-5 rounded mx-auto mt-20 max-w-sm">
+      <Form
+        name="login"
+        layout="vertical"
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <p className="text-2xl text-center">
+          Login to <span className="font-medium">SEFT SPA</span>
+        </p>
+
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="mt-2 w-full">
+            Login
+          </Button>
+        </Form.Item>
+
+        <Form.Item>
+          <Link
+            to={'/signup'}
+            className="text-black hover:text-black hover:underline"
+          >
+            New to <span className="font-medium">SEFT SPA</span>? Sign up now.
+          </Link>
+        </Form.Item>
+      </Form>
+    </Layout>
+  );
 };
 
 export default LoginPage;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,7 +1,57 @@
 import React, { FC } from 'react';
+import { Layout, Form, Input, Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const SignUpPage: FC = () => {
-  return <div>This is Sign Up Page</div>;
+  const onFinish = (values: any) => {
+    console.log('Success :>> ', values);
+  };
+
+  return (
+    <Layout className="p-5 rounded mx-auto mt-20 max-w-sm">
+      <Form
+        name="login"
+        layout="vertical"
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <p className="text-2xl text-center">
+          Create a account on <span className="font-medium">SEFT SPA</span>
+        </p>
+
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="New password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="mt-2 w-full">
+            Sign up
+          </Button>
+        </Form.Item>
+
+        <Form.Item>
+          <Link
+            to={'/login'}
+            className="text-black hover:text-black hover:underline"
+          >
+            Already have an account? Login now.
+          </Link>
+        </Form.Item>
+      </Form>
+    </Layout>
+  );
 };
 
 export default SignUpPage;
